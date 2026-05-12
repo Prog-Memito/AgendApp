16a2715303ad1d252bf637f3f37345986e41fcb3
Joaquin Lecaros <joa.lecaros@duocuc.cl>
2026-05-12 16:44:37 -0400

    Arreglo rut login y visual de contraseña

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,24 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router'; // Importamos el Router para la navegación
+import { Router } from '@angular/router';
 import { 
-  IonContent, 
-  IonHeader, 
-  IonTitle, 
-  IonToolbar, 
-  IonCard, 
-  IonCardHeader, 
-  IonCardTitle, 
-  IonCardContent, 
-  IonLabel, 
-  IonItem, 
-  IonInput, 
-  IonButton, 
-  IonIcon 
+  IonContent, IonCard, IonCardHeader, IonCardTitle, 
+  IonCardContent, IonInput, IonLabel, IonButton, IonIcon 
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { clipboardOutline } from 'ionicons/icons';
+import { eyeOutline, eyeOffOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-login',
@@ -26,35 +15,53 @@ import { clipboardOutline } from 'ionicons/icons';
   styleUrls: ['./login.page.scss'],
   standalone: true,
   imports: [
-    IonContent, 
-    IonHeader, 
-    IonTitle, 
-    IonToolbar, 
-    CommonModule, 
-    FormsModule,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
-    IonLabel,
-    IonItem,
-    IonInput,
-    IonButton,
-    IonIcon
+    CommonModule, FormsModule, IonContent, IonCard, IonCardHeader, 
+    IonCardTitle, IonCardContent, IonInput, IonLabel, IonButton, IonIcon
   ]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  // Inyectamos el Router en el constructor
+  rutValue: string = '';
+  passwordType: string = 'password';
+  showPassword: boolean = false;
+
   constructor(private router: Router) {
-    addIcons({ clipboardOutline });
+    addIcons({ eyeOutline, eyeOffOutline });
   }
 
-  ngOnInit() {}
+  onRutInput(event: any) {
+    let valorRaw = event.target.value.toUpperCase().replace(/[^0-9K]/g, '');
 
-  // Esta función es la que llamaremos desde el botón en el HTML
+    if (valorRaw.includes('K')) {
+      const posicionK = valorRaw.indexOf('K');
+      if (posicionK !== valorRaw.length - 1) {
+        valorRaw = valorRaw.replace(/K/g, '');
+      }
+    }
+
+    if (valorRaw.length > 9) valorRaw = valorRaw.slice(0, 9);
+
+    if (valorRaw.length < 2) {
+      this.rutValue = valorRaw;
+    } else {
+      let cuerpo = valorRaw.slice(0, -1);
+      let dv = valorRaw.slice(-1);
+      cuerpo = cuerpo.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      this.rutValue = `${cuerpo}-${dv}`;
+    }
+    event.target.value = this.rutValue;
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    this.passwordType = this.showPassword ? 'text' : 'password';
+  }
+
+  iniciarSesion() {
+    this.router.navigate(['/home']);
+  }
+
   irAlRegistro() {
     this.router.navigate(['/register']);
   }
-
 }
