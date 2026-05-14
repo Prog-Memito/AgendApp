ca20af3d92eeddd960994d3b5fd8689dcb8df91b
Sebastián Álvarez <sa3776530@gmail.com>
2026-05-14 01:22:19 -0400

    front psicologo

--- src/app/pages/paciente/psicologo/psicologo.page.ts
+++ src/app/pages/paciente/psicologo/psicologo.page.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule, formatDate } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule, IonModal } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { calendarOutline, moonOutline, informationCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-psicologo',
   templateUrl: './psicologo.page.html',
   styleUrls: ['./psicologo.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class PsicologoPage implements OnInit {
+export class PsicologoPage {
+  // Usamos el ID del modal para mayor precisión
+  @ViewChild('modalCalendario') modal!: IonModal; 
 
-  constructor() { }
+  minDate: string = new Date().toISOString();
+  fechaSeleccionada: boolean = false;
+  fechaFormateada: string = '';
+  dateValue: string = ''; 
 
-  ngOnInit() {
+  constructor() {
+    addIcons({ calendarOutline, moonOutline, informationCircleOutline });
+  }
+
+  onDateChange(event: any) {
+    const value = event.detail.value;
+    if (value) {
+      this.dateValue = value;
+      this.fechaSeleccionada = true;
+      
+      // Formateo para las tarjetas de los doctores
+      this.fechaFormateada = formatDate(value, 'EEE dd MMM', 'es-CL');
+      
+      // CERRA EL CALENDARIO INMEDIATAMENTE
+      this.modal.dismiss();
+    }
+  }
+
+  cambiarTipo() {
+    console.log('Cambiando tipo...');
   }
 
 }
