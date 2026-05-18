b06795057c2338c3f4c54ab8634dc3f31581faad
Sebastián Álvarez <sa3776530@gmail.com>
2026-05-18 17:52:54 -0400

    se ordena la casa cambiando de lugar los archivos relacionados con Firebase

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,32 +3,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
-import { getAnalytics } from "firebase/analytics";
 
-// Firebase Standalone Providers
+//Firebase Standalone Providers
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-const firebaseConfig = {
-
-  apiKey: "AIzaSyCqnMoXAQ3JQrc-Pm31eC4WcY_hGzy8SyM",
-
-  authDomain: "agendapp-4f6a0.firebaseapp.com",
-
-  projectId: "agendapp-4f6a0",
-
-  storageBucket: "agendapp-4f6a0.firebasestorage.app",
-
-  messagingSenderId: "348244160042",
-
-  appId: "1:348244160042:web:9f34c13ae346465a859d53",
-
-  measurementId: "G-QBDD77SQPR"
-
-};
+//Traemos el objeto environment
+import { environment } from './environments/environment';
 
 
 bootstrapApplication(AppComponent, {
@@ -36,8 +20,9 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(), // Necesario para conectar con tu DB SQL
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideHttpClient(),
+    // Usamos la configuración centralizada
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
   ],
-});
+}).catch(err => console.log(err));
