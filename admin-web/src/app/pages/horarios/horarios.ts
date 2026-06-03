60283d5861439a855c2d7b5a8ce84530911e905e
Prog-Memito <sa3776530@gmail.com>
2026-06-03 14:56:16 -0400

    Se crea la vista Horarios y desde la misma vista se pueden hacer las horas para que los pacientes puedan agendar sus horas

--- admin-web/src/app/pages/horarios/horarios.ts
+++ admin-web/src/app/pages/horarios/horarios.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { Api } from '../../services/api';
 
 //LLama al Sidebar lateral 
 import { Sidebar } from '../../components/sidebar/sidebar';
@@ -6,8 +10,79 @@ import { Sidebar } from '../../components/sidebar/sidebar';
 @Component({
   selector: 'app-horarios',
   standalone: true,
-  imports: [ Sidebar ],
+  imports: [ Sidebar, CommonModule, FormsModule ],
   templateUrl: './horarios.html',
   styleUrl: './horarios.scss',
 })
-export class Horarios {}
+export class Horarios implements OnInit {
+
+  private api = inject(Api);
+
+  medicos: any[] = [];
+
+  medicoSeleccionado = '';
+  fecha = '';
+  horaInicio = '';
+  horaFin = '';
+  duracion = 30;
+
+  ngOnInit() {
+    this.cargarMedicos();
+  }
+
+  cargarMedicos() {
+    this.api.obtenerMedicos()
+      .subscribe((resp: any) => {
+        this.medicos = resp;
+      });
+  }
+
+  generarHorarios() {
+
+  if (
+    !this.medicoSeleccionado ||
+    !this.fecha ||
+    !this.horaInicio ||
+    !this.horaFin
+  ) {
+
+    alert('Debe completar todos los campos');
+    return;
+
+  }
+
+  const datos = {
+
+    medico: this.medicoSeleccionado,
+    fecha: this.fecha,
+    horaInicio: this.horaInicio,
+    horaFin: this.horaFin,
+    duracion: this.duracion
+
+  };
+
+  this.api.generarHorarios(datos)
+    .subscribe({
+
+      next: (resp: any) => {
+
+        alert(
+          `Se generaron ${resp.horariosGenerados} horarios correctamente`
+        );
+
+      },
+
+      error: (err) => {
+
+        console.error(err);
+
+        alert(
+          'Error al generar los horarios'
+        );
+
+      }
+
+    });
+
+}
+}
